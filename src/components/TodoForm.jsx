b0c81c00570f1667d9d4156f30cc9280fb73c1f6
sorakim <sora6418@gmail.com>
2024-06-24 15:54:49 +0900

    Feat: 1_to_tanstack 제출

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,24 +1,28 @@
 import { useState } from "react";
 import { todoApi } from "../api/todos";
 
-export default function TodoForm({ fetchData }) {
+export default function TodoForm({ data }) {
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
 
   // TODO: useMutation 으로 리팩터링 하세요.
-  const handleAddTodo = async (e) => {
-    e.preventDefault();
-    setTitle("");
-    setContents("");
-    await todoApi.post("/todos", {
-      id: Date.now().toString(),
-      title,
-      contents,
-      isCompleted: false,
-      createdAt: Date.now(),
-    });
-    await fetchData();
-  };
+  // const handleAddTodo = async (e) => {
+  //   e.preventDefault();
+  //   setTitle("");
+  //   setContents("");
+  //   await todoApi.post("/todos", {
+  //     id: Date.now().toString(),
+  //     title,
+  //     contents,
+  //     isCompleted: false,
+  //     createdAt: Date.now(),
+  //   });
+  //   await fetchData();
+  // };
+
+  const useMutation = useMutation({
+
+  }
 
   return (
     <form onSubmit={handleAddTodo}>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {QueryProvider} from "./QueryClientProvier.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryProvider>
+    <App />
+  </QueryProvider>
+);
